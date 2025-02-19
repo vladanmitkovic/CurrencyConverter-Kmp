37b5a9821c0a09ae69fb9e0e7d481939a2e9c9eb
Vladan Mitkovic <vladan@endeavourtechnology.com>
2025-02-19 20:07:01 +0100

    CCKMP: Add linear progress indicator

--- composeApp/src/commonMain/kotlin/me/mitkovic/kmp/currencyconverter/ui/screens/converter/ConverterScreen.kt
+++ composeApp/src/commonMain/kotlin/me/mitkovic/kmp/currencyconverter/ui/screens/converter/ConverterScreen.kt
@@ -1,6 +1,7 @@
 package me.mitkovic.kmp.currencyconverter.ui.screens.converter
 
 import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,19 +9,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import me.mitkovic.kmp.currencyconverter.common.Constants
 import me.mitkovic.kmp.currencyconverter.platform.formatNumber
 import me.mitkovic.kmp.currencyconverter.ui.theme.spacing
@@ -66,6 +77,13 @@ fun ConverterScreen(
                     Text("Go to Favorites")
                 }
             }
+
+            LinearProgressIndicator(state = state, refreshState = refreshState)
+
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = MaterialTheme.colorScheme.surfaceVariant,
+            )
 
             // Ticker holder
             Ticker(
@@ -126,5 +144,47 @@ fun Ticker(
                 style = MaterialTheme.typography.bodySmall,
             )
         }
+    }
+}
+
+@Composable
+fun LinearProgressIndicator(
+    state: ConversionRatesUiState,
+    refreshState: ConversionRatesUiState,
+) {
+    var currentProgress by remember { mutableFloatStateOf(0f) }
+    var loading by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+
+    if ((state.isLoading || refreshState.isLoading) && !loading) {
+        loading = true
+        LaunchedEffect(Unit) {
+            scope.launch {
+                loadProgress { progress ->
+                    currentProgress = progress
+                }
+                loading = false
+            }
+        }
+    }
+    Column(
+        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        if (loading) {
+            LinearProgressIndicator(
+                progress = { currentProgress },
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+/** Iterate the progress value */
+suspend fun loadProgress(updateProgress: (Float) -> Unit) {
+    for (i in 1..100) {
+        updateProgress(i.toFloat() / 100)
+        delay(10)
     }
 }
