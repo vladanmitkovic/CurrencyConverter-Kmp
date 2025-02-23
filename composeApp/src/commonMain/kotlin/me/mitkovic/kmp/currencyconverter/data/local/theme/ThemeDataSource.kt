d73b8f9c1e09ea796483ccf2aa2904ba0137c18c
Vladan Mitkovic <vladan@endeavourtechnology.com>
2025-02-23 19:14:11 +0100

    CCKMP: Changing default theme to be dark

--- composeApp/src/commonMain/kotlin/me/mitkovic/kmp/currencyconverter/data/local/theme/ThemeDataSource.kt
+++ composeApp/src/commonMain/kotlin/me/mitkovic/kmp/currencyconverter/data/local/theme/ThemeDataSource.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface ThemeDataSource {
 
-    suspend fun saveTheme(isDarkMode: Boolean)
+    suspend fun saveTheme(isLightMode: Boolean)
 
     fun getTheme(): Flow<Boolean>
 }
