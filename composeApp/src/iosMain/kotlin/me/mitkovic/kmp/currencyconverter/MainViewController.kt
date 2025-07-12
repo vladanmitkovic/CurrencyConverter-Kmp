e6cf0a7629a2c6bd5cb0b27ff87eb9b3773d5279
Vladan Mitkovic <vladan@endeavourtechnology.com>
2025-07-12 12:15:46 +0200

    CCKMP: Refactoring Koin initialization

--- composeApp/src/iosMain/kotlin/me/mitkovic/kmp/currencyconverter/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/me/mitkovic/kmp/currencyconverter/MainViewController.kt
@@ -1,9 +1,13 @@
 package me.mitkovic.kmp.currencyconverter
 
 import androidx.compose.ui.window.ComposeUIViewController
+import me.mitkovic.kmp.currencyconverter.di.initKoin
 import me.mitkovic.kmp.currencyconverter.ui.App
+import platform.UIKit.UIViewController
 
-fun MainViewController() =
-    ComposeUIViewController {
+fun MainViewController(): UIViewController {
+    initKoin()
+    return ComposeUIViewController {
         App()
     }
+}
